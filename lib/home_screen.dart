de3ff39321bc8cd2ae86c7c4fef0e51af53142a5
Tanvir1943 <tanvir15-14261@diu.edu.bd>
2023-11-03 23:27:12 +0600

    donefinal

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,12 +1,72 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  final List<Map<String, dynamic>> weatherData = [
+    {
+      "city": "New York",
+      "temperature": 20,
+      "condition": "Clear",
+      "humidity": 60,
+      "windSpeed": 5.5,
+    },
+    {
+      "city": "Los Angeles",
+      "temperature": 25,
+      "condition": "Sunny",
+      "humidity": 50,
+      "windSpeed": 6.8,
+    },
+    {
+      "city": "London",
+      "temperature": 15,
+      "condition": "Partly Cloudy",
+      "humidity": 70,
+      "windSpeed": 4.2,
+    },
+    {
+      "city": "Tokyo",
+      "temperature": 28,
+      "condition": "Rainy",
+      "humidity": 75,
+      "windSpeed": 8.0,
+    },
+    {
+      "city": "Sydney",
+      "temperature": 22,
+      "condition": "Cloudy",
+      "humidity": 55,
+      "windSpeed": 7.3,
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(""),
+      appBar: AppBar(
+        title: Text('Weather Information'),
+      ),
+      body: ListView.builder(
+        itemCount: weatherData.length,
+        itemBuilder: (context, index) {
+          final cityData = weatherData[index];
+          return Card(
+            child: ListTile(
+              title: Text('City: ${cityData['city']}'),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Temperature: ${cityData['temperature']}°C'),
+                  Text('Condition: ${cityData['condition']}'),
+                  Text('Humidity: ${cityData['humidity']}%'),
+                  Text('Wind Speed: ${cityData['windSpeed']} km/h'),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
